18c6885771d225c8f37ae9038dd0e6b51fb41e32
0LrNx <FISSSSSS@proton.me>
2025-05-15 12:53:52 +0200

    feat: hero section

---
+++ app/components/Hero/Hero.tsx
@@ -0,0 +1,54 @@
+import BgGrid from "@/public/grid.svg";
+
+import Image from "next/image";
+
+import AvatarList from "@/public/avatar.png";
+
+function Hero({ delaClass }: { delaClass: string }) {
+    return (
+        <div>
+            <div className="bg-[#92C4FE] w-[450px] h-[450px] rounded-full absolute right-96 -top-56 blur-[150px] -z-[500] opacity-80"></div>
+            <div className="bg-[#FEA492] w-[550px] h-[550px] rounded-full absolute -z-[500] left-40 -top-24 blur-[300px] opacity-40"></div>
+
+            <Image
+                src={BgGrid}
+                alt="Background Grid"
+                className="absolute -z-[100] w-full h-full left-[5%] top-[5%] "
+            />
+
+            <div className="flex flex-col items-center justify-center mt-20">
+                <button className="bg-gradient-to-r from-[#E693A5] to-[#3951E5] text-white font-bold font-dmsans rounded-full px-6 py-2">
+                    Introducing a new online selling platform 🎉
+                </button>
+
+                <h1 className={delaClass + " text-6xl font-normal text-center mt-10"}>
+                    Best way to sell <br /> your digital product
+                </h1>
+
+                <p className="mt-10 text-[#626262] text-base font-medium text-center">Go from Zero to Hero with simple platform that helps <br /> creators like you sell their digital products online.</p>
+
+                <div className="flex flex-row mt-10">
+                    <input
+                        type="text"
+                        placeholder="Your best email address"
+                        className="bg-white text-[#626262] font-medium font-dmsans rounded-lg px-6 py-2 h-[50px] placeholder:text-[#626262] placeholder:font-bold placeholder:font-dmsans"
+                    />
+                    <button className="bg-[#314EE7] text-white font-bold font-dmsans rounded-lg px-6 py-2 ml-4">
+                        Try it out
+                    </button>
+                </div>
+
+                <div className="flex flex-row mt-6 w-1/2 mx-auto justify-center items-center">
+                    <Image
+                        src={AvatarList}
+                        alt="Avatar List"
+                        className="w-28 h-full rounded-full border-2 border-white mr-3"
+                    />
+                    <p className="text-[#626262]"><strong className="text-black">More than 100+</strong> users are selling their products <br/> online with simple & easy ways</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Dela_Gothic_One } from "next/font/google";
 
 import Navbar from "@/app/components/Navbar/Navbar";
+import Hero from "@/app/components/Hero/Hero";
 
 const dela = Dela_Gothic_One({ weight: "400", subsets: ["latin"] });
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main>
       <Navbar />
+      <Hero delaClass={dela.className}/>
     </main>
   );
 }
